ded9cea032171c3eed378f67d2042740eb6d4fa8
Blagojche Miskoski <miskoski.blagojce@gmail.com>
2022-06-07 22:21:13 +0200

    added tailwind to project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import AddTodoIcon from "./icons/AddTodoIcon";
+
+const Header = (props) => {
+  return (
+    <nav className="flex justify-between align-center py-5">
+      <h1 className="text-3xl font-bold leading-none">todo</h1>
+      <button onClick={() => props.setToggleForm(true)}>
+        <AddTodoIcon />
+      </button>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/components/icons/DotsIcon.jsx
@@ -0,0 +1,14 @@
+function DotsIcon() {
+  return (
+    <svg
+      width="20px"
+      height="20px"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M10.001 7.8a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 10 7.8zm-7 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 3 7.8zm14 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 17 7.8z" />
+    </svg>
+  );
+}
+
+export default DotsIcon;
